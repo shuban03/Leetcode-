dde50f6a6e00f7140448d44fd89e0b2a511983d6
Shuban P Shetty <shubanpshetty@gmail.com>
2026-07-06 17:30:07 +0530

    Time: 0 ms (100%), Space: 22 MB (44.69%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
@@ -1,31 +1,31 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
     bool isValidBST(TreeNode* root) {
-         return helper(root,NULL,NULL);
-    }
-    bool helper(TreeNode* root,TreeNode* min,TreeNode* max){
-        if(root==NULL) return true;
 
-        //check valid case for min and max
-        if(min!=NULL && root->val<=min->val){
+        // Initially there are no lower or upper limits.
+        return helper(root, NULL, NULL);
+    }
+
+    bool helper(TreeNode* root, TreeNode* min, TreeNode* max) {
+
+        // Empty tree is always a valid BST.
+        if (root == NULL)
+            return true;
+
+        // Current node must be GREATER than the lower bound.
+        if (min != NULL && root->val <= min->val)
             return false;
-        }
-        if(max!=NULL && root->val>=max->val){
+
+        // Current node must be LESS than the upper bound.
+        if (max != NULL && root->val >= max->val)
             return false;
-        }
-        
-        //left and right should be true
-        return helper(root->left,min,root) && helper(root->right,root,max);
+
+        // Left subtree:
+        // Upper bound becomes current node.
+        //
+        // Right subtree:
+        // Lower bound becomes current node.
+        return helper(root->left, min, root) &&
+               helper(root->right, root, max);
     }
 };
