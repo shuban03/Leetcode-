accdb38e3e4c65116789221fbec4516b675da93d
Shuban P Shetty <shubanpshetty@gmail.com>
2026-07-29 17:29:52 +0530

    Time: 7 ms (22.21%), Space: 12.2 MB (30.02%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.cpp
+++ 0133-clone-graph/0133-clone-graph.cpp
@@ -1,20 +1,17 @@
 /*
-Definition for a Node.
+// Definition for a Node.
 class Node {
 public:
     int val;
     vector<Node*> neighbors;
-
     Node() {
         val = 0;
         neighbors = vector<Node*>();
     }
-
     Node(int _val) {
         val = _val;
         neighbors = vector<Node*>();
     }
-
     Node(int _val, vector<Node*> _neighbors) {
         val = _val;
         neighbors = _neighbors;
@@ -24,42 +21,30 @@ public:
 
 class Solution {
 public:
-
     Node* cloneGraph(Node* node) {
-
-        // Map to store: Original Node -> Cloned Node
-        map<Node*, Node*> oldToNew;
-
-        // Start DFS from the given node
-        return dfs(node, oldToNew);
+        unordered_map<Node*,Node*> oldToNew;
+        return dfs(node,oldToNew);
     }
 
-    Node* dfs(Node* node, map<Node*, Node*>& oldToNew) {
-
-        // If graph is empty, return NULL
-        if (node == NULL) {
-            return NULL;
+    Node* dfs(Node* node,unordered_map<Node*,Node*>& oldToNew){
+        //if node is null
+        if(node==nullptr){
+            return nullptr;
         }
 
-        // If node is already cloned, return its copy
-        if (oldToNew.count(node)) {
+        //if node already exists dont create again, just return it
+        if(oldToNew.count(node)){
             return oldToNew[node];
         }
 
-        // Create a new copy of the current node
+        //create the copy node and add to map
         Node* copy = new Node(node->val);
-
-        // Save the mapping before visiting neighbors
-        oldToNew[node] = copy;
-
-        // Clone all neighbors one by one
-        for (Node* nei : node->neighbors) {
-
-            //copy all his neighbors
-            copy->neighbors.push_back(dfs(nei, oldToNew));
+        oldToNew[node]=copy;
+        
+        //copy all the neighbors of the node
+        for(Node* nei: node->neighbors){
+            copy->neighbors.push_back(dfs(nei,oldToNew));
         }
-
-        // Return the cloned node
         return copy;
     }
 };
