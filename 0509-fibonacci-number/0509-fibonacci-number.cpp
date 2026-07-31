09b530b6f54c9cabe55c9658ced31677c6aa76a4
Shuban P Shetty <shubanpshetty@gmail.com>
2026-08-01 01:35:06 +0530

    Time: 3 ms (65.01%), Space: 8.3 MB (11.11%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,12 +1,23 @@
 class Solution {
 public:
     int fib(int n) {
+        vector<int> store(n+1,-1);
+        int ans=solve(n,store);
+        return ans;
+    }
+
+    int solve(int n,vector<int>& store){
         if(n==0){
             return 0;
         }
         if(n==1){
             return 1;
         }
-        return fib(n-1)+fib(n-2);
+
+        if(store[n]!=-1){
+            return store[n];
+        }
+
+        return store[n]=solve(n-1,store)+solve(n-2,store);
     }
 };
