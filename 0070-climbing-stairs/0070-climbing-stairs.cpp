07b1ff493ba2831818ac27cc277ff65b876422a2
Shuban P Shetty <shubanpshetty@gmail.com>
2026-08-01 01:31:05 +0530

    Time: 0 ms (100%), Space: 8.5 MB (38.69%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,7 +1,9 @@
+   
+//using memoiation technizue
     class Solution {
     public:
         int climbStairs(int n) {
-            vector<int> store(n+1,-1);
+            vector<int> store(n+1,-1);       //storage is (n+1) : number starts from 0
             int ans=solve(n,store);
             return ans;
         }
