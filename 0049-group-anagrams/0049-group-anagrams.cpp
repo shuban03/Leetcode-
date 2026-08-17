ff70e166f2d1d8122450e5c08e9fb5852c7b4a60
Shuban P Shetty <shubanpshetty@gmail.com>
2026-08-17 14:58:04 +0530

    Time: 17 ms (53.11%), Space: 26.2 MB (29.61%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -2,14 +2,15 @@ class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         unordered_map<string,vector<string>> map;
+
         for(auto s:strs){
             string k=s;
             sort(k.begin(),k.end());
             map[k].push_back(s);
         }
         vector<vector<string>> res;
-        for(auto m:map){
-            res.push_back(m.second);
+        for(auto ans:map){
+            res.push_back(ans.second);
         }
         return res;
     }
