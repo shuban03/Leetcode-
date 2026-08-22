73bb2cf50934a09f0de866aa5d3e599ad1f1980a
Shuban P Shetty <shubanpshetty@gmail.com>
2026-08-22 21:46:32 +0530

    Time: 0 ms (100%), Space: 20.8 MB (24.23%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,15 +1,10 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        unordered_map<int,int> count;
+        int ans=0;
         for(int num:nums){
-            count[num]++;
+            ans=ans^num;
         }
-        for(auto it:count){
-            if(it.second==1){
-                return it.first;
-            }
-        }
-        return -1;
+        return ans;
     }
 };
